41971b06610e28ef803e31205a01f393f5dd52cd
aldoalfanov <fransiscojehani@gmail.com>
2020-03-23 20:27:58 +0700

    up

---
+++ Mynav/app/src/main/java/com/reynaldo/leo/mynav/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.reynaldo.leo.mynav
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
